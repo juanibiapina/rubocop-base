b6fdd55f57865f303f92e46bcddd99f1e4dd8708
Juan Ibiapina <juanibiapina@gmail.com>
2022-05-20 15:12:45 +0200

    chore: Generate new gem
    ```
    bundle gem rubocop-sensible
    ```

---
+++ lib/rubocop/sensible.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sensible/version"
+
+module Rubocop
+  module Sensible
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
